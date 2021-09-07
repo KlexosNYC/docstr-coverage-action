cc5ba7e4804894d09a0d7c38c56d3a9b1251ba7c
Sean Kazazes <skazazes@klexos.net>
2021-09-07 12:14:49 -0400

    force fail under flag with default 100

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-command="docstr-coverage --verbose $INPUT_VERBOSE "
+command="docstr-coverage --verbose $INPUT_VERBOSE --fail-under $INPUT_FAIL_UNDER"
 
 if [ $INPUT_SKIP_MAGIC ]; then command+="--skip-magic "; fi;
 if [ $INPUT_SKIP_INIT ]; then command+="--skip-init "; fi;
@@ -13,9 +13,8 @@ if [ $INPUT_FOLLOW_LINKS ]; then command+="--follow-links "; fi;
 if [ $INPUT_PERCENTAGE_ONLY ]; then command+="--percentage-only "; fi;
 if [ -n "$INPUT_DOCSTR_IGNORE_FILE" ]; then command+="--docstr-ignore-file $INPUT_DOCSTR_IGNORE_FILE "; fi;
 if [ -n "$INPUT_EXCLUDE" ]; then command+="--exclude $INPUT_EXCLUDE "; fi;
-if [ $INPUT_FAIL_UNDER ]; then command+="--fail-under $INPUT_FAIL_UNDER "; fi;
-
 if [ -n "$GITHUB_WORKSPACE" ]; then command+="$GITHUB_WORKSPACE/"; fi;
+
 command+="$INPUT_SOURCE_DIR"
 
 echo "$command"
